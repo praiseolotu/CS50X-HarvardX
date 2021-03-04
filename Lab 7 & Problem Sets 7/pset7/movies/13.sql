e73fb642cb7ec9fe0caa1a09da59b4dd08b4cb38
Praise Jah <olioxxayo@gmail.com>
2021-03-04 21:30:00 +0100

    Add files via upload

---
+++ Lab 7 & Problem Sets 7/pset7/movies/13.sql
@@ -0,0 +1,1 @@
+SELECT DISTINCT name FROM stars JOIN people ON stars.person_id = people.id WHERE movie_id IN (SELECT movie_id FROM people JOIN stars ON stars.person_id = people.id WHERE name = "Kevin Bacon" AND birth = 1958) AND name != "Kevin Bacon";
